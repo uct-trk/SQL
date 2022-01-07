59d5f74f265218864806ead319fe5fefbdda30a5
Ugurcan Turk <72524087+uct-trk@users.noreply.github.com>
2022-01-07 17:47:37 +0300

    fs-crud

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const daireAlan = require("./daire");
-const daireCevre = require("./daire-cevre");
+const fs = require('fs');
 
-daireAlan(5)
-daireCevre(5)
+fs.writeFile("employees.json","{name: Employee 1 Name, salary: 2000}", "utf8", (err, data) => {
+    if(err) console.log(err)
+    console.log("Employees dosyası oluşturuldu")
+})
+fs.readFile("employees.json", "utf8", (err, data) => {
+    if(err) console.log(err)
+    console.log(data)
+    console.log("dosya okundu")
+})
+fs.writeFile("employees.json","{name: Employee 2 Name, salary: 3000}", "utf8", (err) => {
+    if(err) console.log(err)
+    console.log("dosya içerisindeki veri güncellendi")
+})
+fs.unlink('employees.json', (err) => {
+    if (err) console.log(err)
+    console.log("Dosya silindi")
+})
