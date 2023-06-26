c7f7cd23ed9273c95b8ef8ac0261f0fbdea89b75
UCT <uct@ets-mac-0238.local>
2023-06-27 01:21:59 +0300

    DATA TYPES

---
+++ DATA TYPES.sql
@@ -0,0 +1,20 @@
+--SQL'de kullanılan bazı yaygın veri tipleri şunlardır:
+
+--INTEGER: Tamsayı değerleri temsil eder. Örneğin, 1, 2, -10 gibi.
+
+--FLOAT veya DOUBLE: Ondalıklı sayıları temsil eder. Örneğin, 3.14, -0.5 gibi.
+
+--VARCHAR(n): Değişken uzunluklu karakter dizilerini temsil eder. "n" parametresi, karakter dizisinin maksimum uzunluğunu belirtir.
+
+--CHAR(n): Sabit uzunluklu karakter dizilerini temsil eder. "n" parametresi, karakter dizisinin sabit uzunluğunu belirtir.
+
+--BOOLEAN: Mantıksal değerleri temsil eder. True (doğru) veya False (yanlış) değerlerini alır.
+
+--DATE: Tarih değerlerini temsil eder. Örneğin, '2023-06-23' gibi.
+
+--TIME: Zaman değerlerini temsil eder. Örneğin, '14:30:00' gibi.
+
+--TIMESTAMP: Tarih ve zaman değerlerini temsil eder. Örneğin, '2023-06-23 14:30:00' gibi.
+
+--BLOB: Büyük miktarda binary verileri temsil eder. Örneğin, resimler, videolar gibi verileri saklamak için kullanılır.
+
